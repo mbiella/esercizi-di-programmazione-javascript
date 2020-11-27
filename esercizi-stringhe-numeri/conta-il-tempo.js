79c9279784f7017d32665125a7c3463bdaec1fc7
MAURIZIO BIELLA <maurizio.biella@gmail.com>
2020-11-27 16:45:41 +0100

    Primo commit

--- esercizi-stringhe-numeri/conta-il-tempo.js
+++ esercizi-stringhe-numeri/conta-il-tempo.js
@@ -1,6 +1,7 @@
 /*
   Conta il tempo
-  Scrivi un programma che dato un numero di secondi, calcoli la quantità di ore, minuti e secondi corrispondenti e
+  Scrivi un programma che dato un numero di secondi, calcoli la 
+  quantità di ore, minuti e secondi corrispondenti e
   poi stampi il risultato. L'output avrà solo numeri interi.
 
   Esempio:
@@ -8,7 +9,15 @@
     Output: 3 ore, 29 minuti e 20 secondi.
 
   Consigli:
-  In un'ora ci sono 60 minuti, in un minuto 60 secondi. Quindi quanti secondi ci sono in un'ora? ;)
+  In un'ora ci sono 60 minuti, in un minuto 60 secondi. 
+  Quindi quanti secondi ci sono in un'ora? ;)
 
   http://www.imparareaprogrammare.it
 */
+
+let tempo = 12560;
+let ore = tempo/3600;
+
+
+
+console.log(ore);
